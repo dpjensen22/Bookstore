0c9ef406de2f7fa68fb95b986a3a2b0a5dfecf22
dpjensen22 <dpjensen22@gmail.com>
2022-02-23 21:15:10 -0700

    leftovers + bootstrap

--- Bookstore/Startup.cs
+++ Bookstore/Startup.cs
@@ -57,9 +57,12 @@ namespace Bookstore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            endpoints.MapControllerRoute(
+                name: "Paging",
+                pattern: "Page{pageNum}",
+                defaults: new { Controller = "Home", action = "Index" });
+
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
